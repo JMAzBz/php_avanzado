f13d92dd0a48867a999dbe728fb8a5b30c39caf9
Jose Alvarez <josebenza524@gmail.com>
2026-02-10 22:39:33 -0300

    Codicionales Booleanos

--- index.php
+++ index.php
@@ -3,19 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Variables</title>
+    <title>Condicionales</title>
+    <style>
+    body{
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: Cambria;
+    }
+    </style>
 </head>
 <body>
     <?php
-        /*  ## DIFERENCIA DE LAS COMILLAS "" Y '' ##
-            PARA ANIDAR Y EVALUAR UNA VARIABLE DENTRO DE UNA CADENA SE USA ""
-            PARA IMPRIMIR SOLO CADENAS DE CARACTERES ES RECOMENDABLE USAR ''
-        */
-        $greeting = "Hello";
-
-        echo $greeting.' '. 'Everybody';
-
-        echo "<p> $greeting Everybody </p>";
+        $name = "Dark Matter";
+        $read = false;
+        if($read){
+            $message = "You have read $name.";
+        }else{
+            $message = "You NOT have read $name.";
+        }
     ?>
+    <h1>
+        <?= $message; ?>
+    </h1>
 </body>
 </html>
