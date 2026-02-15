e7b966f87e9d1f36e414af3ca81b6e824daf967d
Jose Alvarez <josebenza524@gmail.com>
2026-02-14 23:22:30 -0300

    Funciones Anonimas

--- index.php
+++ index.php
@@ -1,52 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funciones</title>
+    <title>Funciones Anonimas</title>
 </head>
+
 <body>
     <?php
-        ### HE DEFINIDO UN CÓDIGO FUNCIONAL Y BIEN ESTRUCTURADO PARA EMPEZAR
-        $books =[
-            [
-                'name' => 'Do Androids Dreams of Electric Sheep',
-                'author' => 'Philip k. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2011,
-                'purchaseUrl' => 'http://example.com'
-            ],
-        ];
+    ### HE REFACTORIZADO MI FUNCIONA PARA QUE AHORA SE MÁS GENERICO A LA HORA DE FILTRAR DATOS A
+    ### TRAVES DE FUNCIONES ANONIMAS
+    $books = [
+        [
+            'name' => 'Do Androids Dreams of Electric Sheep',
+            'author' => 'Philip k. Dick',
+            'releaseYear' => 1968,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com'
+        ],
+    ];
 
-        function filterByAuthor($books, $author){
-            $filteredBooks = [];
-            foreach ($books as $book){
-                if($book['author'] === $author){
-                    $filteredBooks[] = $book;
-                }
+    function filter($items, $fn)
+    {
+        $filteredItems = [];
+        foreach ($items as $item) {
+            if ($fn($item)) {
+                $filteredItems[] = $item;
             }
-            return $filteredBooks;
         }
+        return $filteredItems;
+    }
+
+    $filterBooks = filter($books, function($book){
+        return $book['releaseYear'] < 3000;
+    });
     ?>
     <ul>
-        <?php foreach(filterByAuthor($books, 'Andy Weir') as $book) : ?>
-        <li>
-            <a href="<?= $book['purchaseUrl'] ?>">
-                <?=  $book['name'] ?> (<?= $book['releaseYear'] ?>)
-            </a>
-        </li>
+        <?php foreach ($filterBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
 </body>
+
 </html>
