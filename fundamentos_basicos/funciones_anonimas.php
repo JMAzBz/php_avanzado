72e8c64b03d40603e20cc35e426bcd9b94f77fb2
Jose Alvarez <josebenza524@gmail.com>
2026-02-19 18:51:06 -0300

    Manejo de plantillas

---
+++ fundamentos_basicos/funciones_anonimas.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funciones Anonimas</title>
+</head>
+
+<body>
+    <?php
+    ### HE REFACTORIZADO MI FUNCIONA PARA QUE AHORA SE MÁS GENERICO A LA HORA DE FILTRAR DATOS A
+    ### TRAVES DE FUNCIONES ANONIMAS
+    $books = [
+        [
+            'name' => 'Do Androids Dreams of Electric Sheep',
+            'author' => 'Philip k. Dick',
+            'releaseYear' => 1968,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com'
+        ],
+    ];
+
+    function filter($items, $fn)
+    {
+        $filteredItems = [];
+        foreach ($items as $item) {
+            if ($fn($item)) {
+                $filteredItems[] = $item;
+            }
+        }
+        return $filteredItems;
+    }
+    ### AUNQUE RECONOZCO QUE EN PHP YA CONTAMOS UN FILTRO DE DATOS DE ARREGLOS "array_filter"
+    ### ME GUSTO IR UN POCO MÁS ALLA AL REFACTORIZAR Y GENERALIZAR LA FUNCION
+    $filterBooks = filter($books, function($book){
+        return $book['releaseYear'] < 2000;
+    });
+    ?>
+    <ul>
+        <?php foreach ($filterBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,61 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funciones Anonimas</title>
-</head>
-
-<body>
-    <?php
-    ### HE REFACTORIZADO MI FUNCIONA PARA QUE AHORA SE MÁS GENERICO A LA HORA DE FILTRAR DATOS A
-    ### TRAVES DE FUNCIONES ANONIMAS
-    $books = [
-        [
-            'name' => 'Do Androids Dreams of Electric Sheep',
-            'author' => 'Philip k. Dick',
-            'releaseYear' => 1968,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'Project Hail Mary',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2021,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'The Martian',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2011,
-            'purchaseUrl' => 'http://example.com'
-        ],
-    ];
-
-    function filter($items, $fn)
-    {
-        $filteredItems = [];
-        foreach ($items as $item) {
-            if ($fn($item)) {
-                $filteredItems[] = $item;
-            }
-        }
-        return $filteredItems;
-    }
-
-    $filterBooks = filter($books, function($book){
-        return $book['releaseYear'] < 3000;
-    });
-    ?>
-    <ul>
-        <?php foreach ($filterBooks as $book) : ?>
-            <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-
-</html>
+$business = [
+    'name' => 'Laracasts',
+    'cost' => 15,
+    'categories' => ['Testing','php','JavaScript']
+];
+### EL OBJETIVO AQUI FUE LLAMAR A MI ACHIVO DE PLANTILLA PARA IMPRIMIR LOS DATOS DE MI ARREGLO
+require 'index.view.php';
