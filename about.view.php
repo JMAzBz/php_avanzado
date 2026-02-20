de919d70e48188972564c49101eba297a7a033af
Jose Alvarez <josebenza524@gmail.com>
2026-02-20 19:09:15 -0300

    Page Links

--- about.view.php
+++ about.view.php
@@ -20,8 +20,8 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                                <a href="/" aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
-                                <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Abaut</a>
+                                <a href="/" aria-current="page" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Dashboard</a>
+                                <a href="/about.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Abaut</a>
                                 <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
                             </div>
                         </div>
@@ -105,12 +105,12 @@
 
         <header class="relative bg-white shadow-sm">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1>Hello word</h1>
+                <h1>Now you are on the About page.</h1>
             </div>
         </main>
     </div>
---
+++ plantilla-version-01/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$business = [
+    'name' => 'Laracasts',
+    'cost' => 15,
+    'categories' => ['Testing','php','JavaScript']
+];
+### EL OBJETIVO AQUI FUE LLAMAR A MI ACHIVO DE PLANTILLA PARA IMPRIMIR LOS DATOS DE MI ARREGLO
+require 'index.view.php';
